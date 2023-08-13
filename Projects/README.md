c0a170e03dc8f95936e10d63d7c373795423219e
GDSC-ADGITM <142155247+DSC-ADGITM@users.noreply.github.com>
2023-08-13 17:57:54 +0530

    Add files via upload

---
+++ Projects/README.md
@@ -0,0 +1,53 @@
+<div align="center">
+  <h1 align="center">
+    <img src="https://readme-typing-svg.herokuapp.com?color=45ffaa&size=40&width=900&height=80&lines=Welcome-to-Frontend-Projects-World🚀"/>
+  </h1>
+</div>
+
+<div align="center">
+    <h2 style="font-size: 300px;">👨🏻‍💻 PROJECTS AVAILABLE 👨🏻‍💻</h2>
+</div>
+
+<!-- List of all the projects -->
+
+<div align="center">
+
+| No. | 🚀 Project Name | 🔗 Link |
+| --- | --------------- | -------- |
+| 1 | 3D Login Page | [Take Me](./3D%20Login%20Page)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+--- 
+
+<div align="center">
+
+To clone this repository, Go through [README]() file.
+
+<br>
+
+For Contribution, Go through [CONTRIBUTING]() file.
+
+<br>
+
+## If it is beneficial to you, ⭐ star this repository !
+
+</div>
---
+++ projects.js
@@ -0,0 +1,44 @@
+const projects = [
+    {
+        "title":"3D Login Page",
+        "tags":["HTML","CSS","JavaScript"],
+        "img":"img/projects/3D form.png",
+        "description":"This is a 3D SignUp or SignIn page made using HTML, CSS and JavaScript.",
+        "github-link":"#",
+        "project-link":"#"
+    },
+    {
+        "title":"3D Maze",
+        "tags":["HTML","CSS","JavaScript"],
+        "img":"img/projects/3D Maze.png",
+        "description":"This is a 3D maze made using HTML, CSS and JavaScript.It navigates through the maze using the arrow keys on the keyboard. Once the maze is completed, there would be a secret key.",
+        "github-link":"#",
+        "project-link":"#"
+    },
+    {
+        "title":"Air Quality",
+        "tags":["HTML","CSS","JavaScript"],
+        "img":"img/projects/Air_Quality.png",
+        "description":"It is atmospheric Air Quality checker built using HTML, CSS and JavaScript. Based on Latitude and Longitude, it shows the Air Quality Index(concentration of pollutants) of that particular location.",
+        "github-link":"#",
+        "project-link":"#"
+    },
+    {
+        "title":"Analog Clock",
+        "tags":["HTML","CSS","JavaScript"],
+        "img":"img/projects/Analog_Clock.png",
+        "description":"It is analog clock built using HTML, CSS and JavaScript. It shows the current time of the system. It is a simple analog clock with hour, minute and second hands.",
+        "github-link":"#",
+        "project-link":"#"
+    },
+    {
+        "title":"Logistic-Company",
+        "tags":["HTML","CSS","JavaScript"],
+        "img":"img/projects/logiC.png",
+        "description":"It is a Logistic Company landing Page with essential features for a logistic company",
+        "github-link":"#",
+        "project-link":"#"
+    }
+]
+
+export default projects;
