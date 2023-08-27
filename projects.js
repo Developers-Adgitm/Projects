fd467b0cd8573ddac3685aed83808f7a54b419db
Developers-Adgitm <143253346+Developers-Adgitm@users.noreply.github.com>
2023-08-27 21:37:47 +0530

    Update projects.js

--- projects.js
+++ projects.js
@@ -4,48 +4,16 @@ const projects = [
         "tags":["HTML","CSS","JavaScript"],
         "img":"https://images.unsplash.com/photo-1679678691256-fa3ce50c2159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=953&q=80",
         "description":"This is a 3D SignUp or SignIn page made using HTML, CSS and JavaScript.",
-        "github-link":"#",
-        "project-link":"#"
-    },
+        "github-link":"https://github.com/Developers-Adgitm/Projects/tree/main/Projects/3D%20Login%20PAge",
+        "project-link":"https://github.com/Developers-Adgitm/Projects/tree/main/Projects/Calculator"
+    },    
     {
-        "title":"3D Maze",
+        "title":"Calculator",
         "tags":["HTML","CSS","JavaScript"],
-        "img":"https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=874&q=80",
-        "description":"This is a 3D maze made using HTML, CSS and JavaScript.It navigates through the maze using the arrow keys on the keyboard. Once the maze is completed, there would be a secret key.",
-        "github-link":"#",
-        "project-link":"#"
-    },
-    {
-        "title":"Air Quality",
-        "tags":["HTML","CSS","JavaScript"],
-        "img":"https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
-        "description":"It is atmospheric Air Quality checker built using HTML, CSS and JavaScript. Based on Latitude and Longitude, it shows the Air Quality Index(concentration of pollutants) of that particular location.",
-        "github-link":"#",
-        "project-link":"#"
-    },
-    {
-        "title":"Analog Clock",
-        "tags":["HTML","CSS","JavaScript"],
-        "img":"https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
-        "description":"It is analog clock built using HTML, CSS and JavaScript. It shows the current time of the system. It is a simple analog clock with hour, minute and second hands.",
-        "github-link":"#",
-        "project-link":"#"
-    },
-    {
-        "title":"Logistic-Company",
-        "tags":["HTML","CSS","JavaScript"],
-        "img":"https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
-        "description":"It is a Logistic Company landing Page with essential features for a logistic company",
-        "github-link":"#",
-        "project-link":"#"
-    },
-    {
-        "title":"Animated Card",
-        "tags":["HTML","CSS"],
-        "img":"https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
-        "description":"It is animated card built using HTML,CSS.",
-        "github-link":"#",
-        "project-link":"#"
+        "img":"https://images.unsplash.com/photo-1679678691256-fa3ce50c2159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=953&q=80",
+        "description":"This is a Calculator made using HTML, CSS and JavaScript.",
+        "github-link":"https://github.com/Developers-Adgitm/Projects/tree/main/Projects/Calculator",
+        "project-link":"https://github.com/Developers-Adgitm/Projects/tree/main/Projects/Calculator"
     },
     
 ]
