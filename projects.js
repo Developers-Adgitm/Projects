b9e6957a3968378ce5c8afc4a3e1220ef6b027f5
DSC-ADGITM <143253346+Developers-Adgitm@users.noreply.github.com>
2023-09-06 00:58:18 +0530

    Update projects.js

--- projects.js
+++ projects.js
@@ -1,8 +1,8 @@
 const projects = [
-    {
+    {   
         "title":"3D Login Page",
         "tags":["HTML","CSS","JavaScript"],
-        "img":"https://images.unsplash.com/photo-1679678691256-fa3ce50c2159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=953&q=80",
+        "img":"https://picsum.photos/200/300",
         "description":"This is a 3D SignUp or SignIn page made using HTML, CSS and JavaScript.",
         "github-link":"https://github.com/Developers-Adgitm/Projects/tree/main/Projects/3D%20Login%20PAge",
         "project-link":"https://github.com/Developers-Adgitm/Projects/tree/main/Projects/Calculator"
@@ -10,7 +10,7 @@ const projects = [
     {
         "title":"Calculator",
         "tags":["HTML","CSS","JavaScript"],
-        "img":"https://images.unsplash.com/photo-1679678691256-fa3ce50c2159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=953&q=80",
+        "img":"https://picsum.photos/200/300",
         "description":"This is a Calculator made using HTML, CSS and JavaScript.",
         "github-link":"https://github.com/Developers-Adgitm/Projects/tree/main/Projects/Calculator",
         "project-link":"https://github.com/Developers-Adgitm/Projects/tree/main/Projects/Calculator"
@@ -18,7 +18,7 @@ const projects = [
     {
         "title":"Twitter Sentiment Analysis",
         "tags":["Python"],
-        "img":"https://images.unsplash.com/photo-1679678691256-fa3ce50c2159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=953&q=80",
+        "img":"https://picsum.photos/200/300",
         "description":"This is a Twitter Sentiment Analysis made using Python.",
         "github-link":"https://github.com/Developers-Adgitm/Projects/blob/main/Projects/Twitter_Sentiment_Analysis.ipynb",
         "project-link":"https://github.com/Developers-Adgitm/Projects/blob/main/Projects/Twitter_Sentiment_Analysis.ipynb"
