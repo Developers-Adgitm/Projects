9d5562171f59991291fa9d9cf60aa1dac8f978b6
DSC-ADGITM <143253346+Developers-Adgitm@users.noreply.github.com>
2023-09-06 01:02:07 +0530

    Update projects.js

--- projects.js
+++ projects.js
@@ -2,7 +2,7 @@ const projects = [
     {   
         "title":"3D Login Page",
         "tags":["HTML","CSS","JavaScript"],
-        "img":"https://picsum.photos/200/300",
+        "img":"img/projects/3D form.png",
         "description":"This is a 3D SignUp or SignIn page made using HTML, CSS and JavaScript.",
         "github-link":"https://github.com/Developers-Adgitm/Projects/tree/main/Projects/3D%20Login%20PAge",
         "project-link":"https://github.com/Developers-Adgitm/Projects/tree/main/Projects/Calculator"
