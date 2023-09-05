07da78284ac9a299f943a406385a2eb57ed57917
DSC-ADGITM <143253346+Developers-Adgitm@users.noreply.github.com>
2023-09-06 00:53:51 +0530

    Update projects.js

--- projects.js
+++ projects.js
@@ -14,6 +14,14 @@ const projects = [
         "description":"This is a Calculator made using HTML, CSS and JavaScript.",
         "github-link":"https://github.com/Developers-Adgitm/Projects/tree/main/Projects/Calculator",
         "project-link":"https://github.com/Developers-Adgitm/Projects/tree/main/Projects/Calculator"
+    }, 
+    {
+        "title":"Twitter Sentiment Analysis",
+        "tags":["Python"],
+        "img":"https://images.unsplash.com/photo-1679678691256-fa3ce50c2159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=953&q=80",
+        "description":"This is a Twitter Sentiment Analysis made using Python.",
+        "github-link":"https://github.com/Developers-Adgitm/Projects/blob/main/Projects/Twitter_Sentiment_Analysis.ipynb",
+        "project-link":"https://github.com/Developers-Adgitm/Projects/blob/main/Projects/Twitter_Sentiment_Analysis.ipynb"
     },
     
 ]
