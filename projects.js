a00d71771049455937a81786753298d096a155d3
VEER YADAV <112725639+codingwithveer18@users.noreply.github.com>
2023-10-08 16:27:41 +0530

    Update projects.js

--- projects.js
+++ projects.js
@@ -23,6 +23,30 @@ const projects = [
         "github-link":"https://github.com/Developers-Adgitm/Projects/blob/main/Projects/Twitter_Sentiment_Analysis.ipynb",
         "project-link":"https://github.com/Developers-Adgitm/Projects/blob/main/Projects/Twitter_Sentiment_Analysis.ipynb"
     },
+    {
+        "title":"AIML-Data_Science Project",
+        "tags":["Python"],
+        "img":"https://learn.g2.com/hubfs/G2CM_FI454_Learn_Article_Images_%5BFacial_recognition%5D_V1a-1.png",
+        "description":"This is Facial-Verification-with-Siamese-Network & Image_Classification using Python.",
+        "github-link":"https://github.com/Developers-Adgitm/Projects/tree/main/Projects/AIML-DATA-SCIENCE-PROJECTS",
+        "project-link":"https://github.com/Developers-Adgitm/Projects/tree/main/Projects/AIML-DATA-SCIENCE-PROJECTS"
+    },
+    {
+        "title":"Downloadify",
+        "tags":["React", "Flask"],
+        "img":"https://downloadify.vercel.app/",
+        "description":"This is a Twitter Sentiment Analysis made using Python.",
+        "github-link":"https://github.com/Developers-Adgitm/Projects/tree/main/Projects/downloadify",
+        "project-link":"https://downloadify.vercel.app/"
+    },
+    {
+        "title":"ifeelgreen",
+        "tags":["HTML","CSS","JavaScript"],
+        "img":"https://editor.analyticsvidhya.com/uploads/50131cover.jpg",
+        "description":"This helps you get started with organic lifestyle",
+        "github-link":"https://github.com/Developers-Adgitm/Projects/blob/main/Projects/Twitter_Sentiment_Analysis.ipynb",
+        "project-link":"https://sl1ck0.github.io/ifeelgreen.github.io/"
+    },
     
 ]
 
